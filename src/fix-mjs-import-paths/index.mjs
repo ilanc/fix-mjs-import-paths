675ebda06c21e88dee76f92258bfbaf4536963f7
ilan <ilan@spikedata.co.za>
2020-05-07 15:28:08 +0200

    lo named export

--- src/fix-mjs-import-paths/index.mjs
+++ src/fix-mjs-import-paths/index.mjs
@@ -2,7 +2,7 @@ import path from "path"; // bare import
 import defaultDep from "./defaultDep"; // extensionless default import
 import * as namedDep from "./namedDep"; // extensionless named import
 import folderIndex from "./folder"; // folder index i.e. folder/index.mjs
-import lo from "./lodashUser"; // folder index i.e. folder/index.mjs
+import * as lo from "./lodashUser"; // folder index i.e. folder/index.mjs
 
 console.log("path.sep", path.sep);
 console.log("defaultDep", JSON.stringify(defaultDep));
